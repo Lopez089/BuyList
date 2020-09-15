2a62a4d58d870fab8ff5941fb77092ecba0a1e5e
Juan <aragonlopezjuan87@gmail.com>
2020-09-15 14:26:12 +0200

    layout app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import ContentList from "./components/ContentList/contentList";
+
+import { ArticleList } from "./components/ContentList/contentList.stories";
 
 function App() {
-    return <div>app</div>;
+    return (
+        <>
+            <ContentList titleList="Buy List" typeList="buyList" articleList={ArticleList} />
+            <ContentList titleList="All List" typeList="AllList" articleList={ArticleList} />
+        </>
+    );
 }
 
 export default App;
